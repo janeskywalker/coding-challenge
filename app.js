380e935a3754c5b737c32b655c56aa89618f9c40
Jane Skywalker <janehuizeng@gmail.com>
2019-12-07 12:35:30 -0800

    adding solution

--- app.js
+++ app.js
@@ -3947,5 +3947,34 @@ var moveZeroes = function(nums) {
    
     return nums
 };
-console.log(moveZeroes([0,1,0,3,12])) //[1,3,12,0,0]
+// console.log(moveZeroes([0,1,0,3,12])) //[1,3,12,0,0]
 // console.log(moveZeroes([0,0,1])) //[1,3,12,0,0]
+
+
+
+
+
+
+
+/**
+ * There are a row of n houses, each house can be painted with one of the three colors: red, blue or green. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+
+The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, costs[0][0] is the cost of painting house 0 with color red; costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum cost to paint all houses.
+
+All costs are positive integers.
+ */
+
+var minCost = function(costs) {
+    var n = costs.length -1
+    for(let i=1; i<costs.length; i++) {
+        costs[i][1] += Math.min(costs[i-1][0], costs[i-1][2])
+        costs[i][0] += Math.min(costs[i-1][1], costs[i-1][2])
+        costs[i][2] += Math.min(costs[i-1][0], costs[i-1][1])
+        // console.log(costs[n][0])
+        // console.log(costs[n][1])
+        // console.log(costs[n][2])
+    }
+
+    return Math.min(costs[n][0], costs[n][1], costs[n][2])
+}
+console.log(minCost([[17,2,17],[16,16,5],[14,3,19]])) // 10 (2+5+3)
