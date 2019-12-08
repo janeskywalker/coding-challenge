4c3f5989d528c807025fc9ca7ca3ffe7527af0a0
Jane Skywalker <janehuizeng@gmail.com>
2019-12-07 17:01:27 -0800

    adding solution

--- app.js
+++ app.js
@@ -3962,19 +3962,25 @@ var moveZeroes = function(nums) {
 The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, costs[0][0] is the cost of painting house 0 with color red; costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum cost to paint all houses.
 
 All costs are positive integers.
+
+house1: r, g, b
+house2: r, g, b
+house3: r, g, b
+
  */
 
 var minCost = function(costs) {
+    if (!costs.length) return 0;
+    
     var n = costs.length -1
     for(let i=1; i<costs.length; i++) {
-        costs[i][1] += Math.min(costs[i-1][0], costs[i-1][2])
         costs[i][0] += Math.min(costs[i-1][1], costs[i-1][2])
+        costs[i][1] += Math.min(costs[i-1][0], costs[i-1][2])
         costs[i][2] += Math.min(costs[i-1][0], costs[i-1][1])
-        // console.log(costs[n][0])
-        // console.log(costs[n][1])
-        // console.log(costs[n][2])
+        console.log(costs[i][0])
+        console.log(costs[i][1])
+        console.log(costs[i][2])
     }
-
     return Math.min(costs[n][0], costs[n][1], costs[n][2])
 }
 console.log(minCost([[17,2,17],[16,16,5],[14,3,19]])) // 10 (2+5+3)
