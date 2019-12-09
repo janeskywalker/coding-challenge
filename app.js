cce142647207bce7e0f28657f3cabaabd0bc6795
Jane Skywalker <janehuizeng@gmail.com>
2019-12-09 15:29:27 -0800

    adding solution

--- app.js
+++ app.js
@@ -4083,3 +4083,71 @@ var isAlienSorted = function(words, order) {
 // console.log(isAlienSorted(["hello","leetcode"], "hlabcdefgijkmnopqrstuvwxyz")) // true
 // console.log(isAlienSorted(["word","world","row"], "worldabcefghijkmnpqstuvxyz")) // false
 // console.log(isAlienSorted(["apple","app"], "abcdefghijklmnopqrstuvwxyz")) // false
+
+
+
+
+
+
+
+/**
+ * In a given grid, each cell can have one of three values:
+
+the value 0 representing an empty cell;
+the value 1 representing a fresh orange;
+the value 2 representing a rotten orange.
+Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
+
+Return the minimum number of minutes that must elapse until no cell has a fresh orange.  If this is impossible, return -1 instead.
+ */
+
+var orangesRotting = function(grid) {
+    // queue is to store rotten oranges indices of row and column
+    let queue = []
+    let fresh = 0
+    let minutes = 0
+
+    // scan to find rotten orange indices and fresh orange count
+    for(let i=0; i<grid.length; i++) {
+        for(let j=0; j<grid[0].length; j++) {
+            if(grid[i][j] === 1) {
+                fresh++
+            } else if(grid[i][j] === 2) {
+                queue.push([i,j])
+            }
+        }
+    }
+    // classic bfs
+    // keep checking till queue is empty, meaning no more rotten one to begin with, or no fresh ones left
+    while(queue.length !== 0 && fresh) {
+        let dR = [0, -1, 0, 1]
+        let dC = [-1, 0, 1, 0]
+        let next = []
+        // you dont add minues till one layer is finished, finish one depth first
+        // this queue is a different queue
+        while(queue.length !== 0) {
+            // pop one rotten orange out
+            let curRotten = queue.shift()
+            // check this rotten orange's 4 directions 
+            for(let i=0; i<dR.length; i++) {
+                let nR = curRotten[0] + dR[i]
+                let nC = curRotten[1] + dC[i]
+                if( nR >= 0 && nC >=0 && nR < grid.length && nC < grid[0].length ) {
+                    if(grid[nR][nC] === 1) {
+                        grid[nR][nC] = 2
+                        fresh--
+                        next.push([nR, nC])
+                    }
+                }
+            }
+        }
+
+        // after checking all 4 directions, increment minutes
+        minutes++
+        queue = next
+    }
+    return fresh === 0 ? minutes : -1
+}
+// console.log(orangesRotting([[2,1,1],[1,1,0],[0,1,1]])) //4
+// console.log(orangesRotting([[2,1,1],[0,1,1],[1,0,1]])) //-1
+console.log(orangesRotting([[0,2]])) //0
