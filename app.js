034be710b24ea9be6a8849a4db837d5ec1a6ff42
Jane Skywalker <janehuizeng@gmail.com>
2019-12-09 20:44:50 -0800

    adding recursive solution

--- app.js
+++ app.js
@@ -4049,7 +4049,7 @@ var minCost = function(costs) {
  * Given two arrays, write a function to compute their intersection.
  */
 
-
+// use set to optimize the solution, instead of O(N*M), it is 0(N)
 var intersection = function(nums1, nums2) {
     const set1 = new Set(nums1)
     const set2 = new Set(nums2)
@@ -4141,6 +4141,10 @@ var isAlienSorted = function(words, order) {
 // console.log(isAlienSorted(["hello","leetcode"], "hlabcdefgijkmnopqrstuvwxyz")) // true
 // console.log(isAlienSorted(["word","world","row"], "worldabcefghijkmnpqstuvxyz")) // false
 // console.log(isAlienSorted(["apple","app"], "abcdefghijklmnopqrstuvwxyz")) // false
+
+
+
+
 
 
 
@@ -4296,4 +4300,108 @@ var isPalindromeNum = function(x) {
 
 
 
+
+
+
+
+/**
+ * Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+1(square) + 9(sqare) = 82
+8(square)+ 2(square) = 68
+6(square) + 8(square) = 100
+12 + 0(square) + 0(square) = 1
+ */
+
+ // recursion w/ try/catch statement
+var isHappy2 = function(n) {
+    let str = n.toString()
+    let sum = 0
+    for(let char of str) {
+        let digit = Number(char)
+        sum += Math.pow(digit, 2)
+    }
+
+    if(sum === 1) {
+        return true
+    }
+
+    //the return false statement is only executed after a TLE error. But demonstrating one way of solving this in case you weren't able to immediately come up with the optimal approach.
+    try {
+        return isHappy2(sum);
+    } catch(e) {
+        return false;
+    }
+}
+
+
+
+
+
+
+
+// iterative 
+// var isHappy = function(n) {
+//     const set = new Set
+
+//     while(n !== 1) {
+//         let current = n
+//         let sum = 0
+//         while(current !== 0) {
+//             sum += (current % 10) * (current % 10)
+//             current = Math.floor(current/10)
+//         }
+
+//         if(set.has(sum)) {
+//             return false
+//         } 
+
+//         set.add(sum)
+//         n = sum
+//     }
+
+//         return true
+// };
+
+
+
+
+// recursion with set to keep track on sum -- very proud!
+var isHappy = function(n) {
+    let set = new Set
+
+    const check = (n) => {
+        let sum = 0
+        let current = n
+        while(current !== 0) {
+            sum += (current % 10) * (current % 10)
+            current = Math.floor(current/10)
+        }
+
+        if(sum === 1) {
+            return true
+        } else {
+            if(set.has(sum)) {
+                return false
+            }
+            set.add(sum)
+            return check(sum)
+        }
+    }
+
+    return check(n)
+}
+
+
+
+console.log(isHappy(19)) //true
+console.log(isHappy(116)) //false
+console.log(isHappy(7)) //true
 
